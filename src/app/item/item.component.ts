94208e9d8649391291916444e986bbe123e12a43
nicolas <jaljordan77@gmail.com>
2023-08-09 17:15:12 -0500

    update->src/app/item/item.component.ts

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -33,11 +33,10 @@ export class ItemComponent {
       description: desc,
       responsible: responsable,
       dueData: date,
-      done: false
+      done:false
     }
     console.log(this.Item._id)
-    // si actualiza pero falta arreglar el bug de app.component, el back al crear una tarea debe responder con la tarea que contenga el id para asignarla a allitems y que se pueda usar el id para borrar
-    // si se crea una tarea solo se puede actualizar o borrar si recargas la pagina primero ya que el back vuelve a responder la data con el id.
+    
       this.TodoService.updateTask('https://todolist-vp6o.onrender.com/todo/update/' + this.Item._id,updatedTask).subscribe(
         (data) => {console.log(data)}
       )
